83793c53adaf09fb0f23e850a90db90856491595
Percy Lackgren <p.lackgren@rgu.ac.uk>
2023-02-23 10:51:19 +0000

    Added react-routing

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import EmployeeForm from "./components/EmployeeForm"
+import ResourceForm from "./components/ResourceForm"
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <EmployeeForm></EmployeeForm>
+            <ResourceForm></ResourceForm>;
+        </div> 
+    )
+  };
+  
+  export default Home;
