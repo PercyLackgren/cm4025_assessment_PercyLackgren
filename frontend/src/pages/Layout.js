fd043978ec8b535a1fba4adf4eb0ba9aadd1ed09
Percy Lackgren <p.lackgren@rgu.ac.uk>
2023-03-29 17:39:49 +0100

    Styled login, redid database schemas

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,16 +1,32 @@
 import { Outlet, Link } from "react-router-dom";
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-import React, { setState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 axios.defaults.withCredentials = true
 
 function Layout() {
+  
+  // Authenticate user in layout, then pass the user to outlet context if any.
+  const [authenticatedUser, setAuthenticatedUser] = useState(null);
+  
+  useEffect(() => {
+    async function checkAuthentication() {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/users/profile');
+        setAuthenticatedUser(response.data);
+        // console.log(response.data);
+      } catch (error) {
+        // console.log(error.response.data);
+      }
+    }
+    checkAuthentication();
+  }, []);
 
+  
   return (
     <>
       <Navbar bg="light" variant="light">
@@ -20,11 +36,11 @@ function Layout() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/quote">Create Quote</Nav.Link>
           </Nav>
-          <Button href="/signin" variant="outline-primary" className="justify-content-end">Sign in</Button>
+          <Button href="/user" variant="outline-primary" className="justify-content-end">Sign in</Button>
         </Container>
       </Navbar>
 
-      <Outlet />
+      <Outlet context={authenticatedUser}/>
     </>
   )
 };
