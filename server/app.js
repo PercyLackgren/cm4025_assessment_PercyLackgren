96c643da9fd60a27279f984c379a628cd74d8fe7
Percy Lackgren <p.lackgren@rgu.ac.uk>
2023-12-05 16:18:25 +0100

    add CORS for vercel

--- server/app.js
+++ server/app.js
@@ -64,7 +64,7 @@ app.use(express.json({
 // Enable CORS for all routes
 const cors = require('cors');
 app.use(cors({ 
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://cm4025-assessment-percy-lackgren.vercel.app/'],
     credentials: true 
 }));
 
